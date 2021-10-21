06258dcb1535fc39c11595b7b3a04fc42ee09a49
redshiftzero <jen@penumbra.zone>
2021-10-21 13:34:12 -0400

    feat: add rate 3 instance (for nullifier derivation)

--- src/hash.rs
+++ src/hash.rs
@@ -25,6 +25,20 @@ pub fn hash_2(domain_separator: &Fq, value: (Fq, Fq)) -> Fq {
     state[1]
 }
 
+/// Hash three [`Fq`] elements with the provided `domain_separator`.
+pub fn hash_3(domain_separator: &Fq, value: (Fq, Fq, Fq)) -> Fq {
+    let mut state = State::from(crate::RATE_3_PARAMS.clone());
+
+    // Use the domain separator as the sponge's capacity element
+    state[0] = domain_separator.clone();
+    state[1] = value.0;
+    state[2] = value.1;
+    state[3] = value.2;
+
+    state.permute();
+    state[1]
+}
+
 /// Hash four [`Fq`] elements with the provided `domain_separator`.
 pub fn hash_4(domain_separator: &Fq, value: (Fq, Fq, Fq, Fq)) -> Fq {
     let mut state = State::from(crate::RATE_4_PARAMS.clone());
