a3c9612f933ddcfbe8c78e28614fcbe060b60a01
redshiftzero <jen@penumbra.zone>
2022-06-02 11:17:01 -0400

    doc: add a README, further code docs (#13)

--- poseidon-paramgen/src/mds.rs
+++ poseidon-paramgen/src/mds.rs
@@ -17,7 +17,7 @@ where
             panic!("no MDS matrix exists");
         }
 
-        MdsMatrix::fixed_cauchy_matrix(&input)
+        MdsMatrix::fixed_cauchy_matrix(input)
     }
 
     /// Dimension of the (square) MDS matrix
@@ -28,17 +28,18 @@ where
     /// Generate a deterministic Cauchy matrix
     ///
     /// The original Poseidon paper describes a method for constructing MDS matrices
-    /// from randomly selecting $x_i$, $y_j$ from the field. The resulting matrix needs
-    /// to be passed to algorithms 1-3 described in
+    /// from randomly selecting $x_i$, $y_j$ from the field and then constructing each element in the
+    /// matrix using $1/(x_i + y_j)$. The resulting Cauchy matrix needs to then be passed to algorithms 1-3
+    /// described in
     /// [Grassi, Rechberger, Schofnegger 2020](https://eprint.iacr.org/archive/2020/500/20200702:141143)
     /// in order to determine if infinitely long subspace trails can be constructed for
     /// the Cauchy matrix. If yes, then the MDS matrix must be thrown away, and the process
-    /// must begin again for another random choice of $x_i$, $y_j$.
+    /// must begin again for another random choice of $x_i$, $y_j$ until a secure choice is found.
     ///
     /// However, Section 5.4 of [Keller and Rosemarin 2020](https://eprint.iacr.org/2020/179.pdf)
     /// describes how the MDS matrix can be constructed in a deterministic fashion
     /// where infinitely long subspace trails cannot be constructed. This method constructs an MDS
-    /// matrix using that method.
+    /// matrix using that deterministic method, avoiding the need to implement Algorithms 1-3.
     pub fn fixed_cauchy_matrix(input: &InputParameters<F::BigInt>) -> Self {
         let xs: Vec<F> = (0..input.t as u64).map(F::from).collect();
         let ys: Vec<F> = (input.t as u64..2 * input.t as u64).map(F::from).collect();
