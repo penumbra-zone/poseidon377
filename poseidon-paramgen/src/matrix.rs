0633ad1f9b0f52f9c9a9203619ff92aa68593cd6
redshiftzero <jen@penumbra.zone>
2022-07-05 21:17:20 -0400

    perf: optimized MDS - M''

--- poseidon-paramgen/src/matrix.rs
+++ poseidon-paramgen/src/matrix.rs
@@ -1,5 +1,6 @@
 use std::ops::Mul;
 
+use anyhow::{anyhow, Result};
 use ark_ff::PrimeField;
 
 /// Represents a matrix over `PrimeField` elements
@@ -28,6 +29,22 @@ impl<F: PrimeField> Matrix<F> {
 
     pub fn set_element(&mut self, i: usize, j: usize, val: F) {
         self.elements[i * self.n_cols + j] = val
+    }
+
+    pub fn rows(&self) -> Vec<&[F]> {
+        self.elements.chunks(self.n_cols).collect()
+    }
+
+    /// Take transpose of the matrix
+    pub fn transpose(&self) -> Matrix<F> {
+        let mut transposed_elements = Vec::with_capacity(self.n_rows * self.n_cols);
+
+        for j in 0..self.n_cols {
+            for i in 0..self.n_rows {
+                transposed_elements.push(self.get_element(i, j))
+            }
+        }
+        Matrix::new(self.n_cols, self.n_rows, transposed_elements)
     }
 }
 
@@ -259,6 +276,10 @@ fn flatten<T>(nested: Vec<Vec<T>>) -> Vec<T> {
 
 /// Compute dot product between two vectors
 pub fn dot_product<F: PrimeField>(a: Vec<F>, b: Vec<F>) -> F {
+    if a.len() != b.len() {
+        panic!("vecs not same len")
+    }
+
     a.iter().zip(b.iter()).map(|(x, y)| *x * *y).sum()
 }
 
@@ -267,8 +288,8 @@ impl<F: PrimeField> Mul<SquareMatrix<F>> for SquareMatrix<F> {
     type Output = SquareMatrix<F>;
 
     // Only multiplying square matrices is infallible
-    // since the number of rows in the LHS must be equal to the
-    // number of columns in the RHS.
+    // since the number of columns in the LHS must be equal to the
+    // number of rows in the RHS.
     fn mul(self, rhs: Self) -> Self::Output {
         let rhs_T = rhs.transpose();
 
@@ -287,6 +308,31 @@ impl<F: PrimeField> Mul<SquareMatrix<F>> for SquareMatrix<F> {
 
         SquareMatrix::from_vec(flatten(res))
     }
+}
+
+pub fn mat_mul<F: PrimeField>(lhs: &Matrix<F>, rhs: &Matrix<F>) -> Result<Matrix<F>> {
+    if lhs.n_cols != rhs.n_rows {
+        return Err(anyhow!(
+            "matrix dimensions do not allow matrix multiplication"
+        ));
+    }
+
+    let rhs_T = rhs.transpose();
+
+    let res: Vec<Vec<F>> = lhs
+        .rows()
+        .into_iter()
+        .map(|row| {
+            // Rows of the transposed matrix are the columns of the original matrix
+            rhs_T
+                .rows()
+                .into_iter()
+                .map(|column| dot_product(row.to_vec(), column.to_vec()))
+                .collect()
+        })
+        .collect();
+
+    Ok(Matrix::new(lhs.n_rows, rhs.n_cols, flatten(res)))
 }
 
 /// Matrix multiplication
@@ -326,7 +372,7 @@ mod tests {
     }
 
     #[test]
-    fn matmul() {
+    fn square_matmul() {
         let identity: SquareMatrix<Fq> = SquareMatrix::identity(2);
 
         let matrix_2x2 = SquareMatrix::from_vec(vec![
@@ -344,7 +390,59 @@ mod tests {
     }
 
     #[test]
+    fn nonsquare_matmul() {
+        let test_elements = vec![
+            Fq::one(),
+            Fq::from(2u64),
+            Fq::from(3u64),
+            Fq::from(4u64),
+            Fq::from(5u64),
+            Fq::from(6u64),
+        ];
+        let matrix_2x3 = Matrix::new(3, 2, test_elements);
+
+        let res = mat_mul(&matrix_2x3, &matrix_2x3);
+        assert!(res.is_err());
+
+        let matrix_3x2 = matrix_2x3.transpose();
+        let res = mat_mul(&matrix_2x3, &matrix_3x2).expect("is ok");
+        assert_eq!(res.get_element(0, 0), Fq::from(5u64));
+        assert_eq!(res.get_element(0, 1), Fq::from(11u64));
+        assert_eq!(res.get_element(0, 2), Fq::from(17u64));
+        assert_eq!(res.get_element(1, 0), Fq::from(11u64));
+        assert_eq!(res.get_element(1, 1), Fq::from(25u64));
+        assert_eq!(res.get_element(1, 2), Fq::from(39u64));
+        assert_eq!(res.get_element(2, 0), Fq::from(17u64));
+        assert_eq!(res.get_element(2, 1), Fq::from(39u64));
+        assert_eq!(res.get_element(2, 2), Fq::from(61u64));
+    }
+
+    #[test]
     fn transpose() {
+        let matrix_2x3 = Matrix::new(
+            3,
+            2,
+            vec![
+                Fq::one(),
+                Fq::from(2u64),
+                Fq::from(3u64),
+                Fq::from(4u64),
+                Fq::from(5u64),
+                Fq::from(6u64),
+            ],
+        );
+        assert_eq!(matrix_2x3.get_element(0, 1), Fq::from(2u64));
+        assert_eq!(matrix_2x3.get_element(1, 0), Fq::from(3u64));
+        assert_eq!(matrix_2x3.get_element(1, 1), Fq::from(4u64));
+        assert_eq!(matrix_2x3.get_element(2, 0), Fq::from(5u64));
+        assert_eq!(matrix_2x3.get_element(2, 1), Fq::from(6u64));
+        let res = matrix_2x3.transpose();
+        assert_eq!(res.get_element(1, 0), Fq::from(2u64));
+        assert_eq!(res.get_element(0, 1), Fq::from(3u64));
+        assert_eq!(res.get_element(1, 1), Fq::from(4u64));
+        assert_eq!(res.get_element(0, 2), Fq::from(5u64));
+        assert_eq!(res.get_element(1, 2), Fq::from(6u64));
+
         let matrix_2x2 = SquareMatrix::from_vec(vec![
             Fq::one(),
             Fq::from(2u64),
