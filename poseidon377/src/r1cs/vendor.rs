b98ff0b559e88c11a5ede205832c43b2632cee30
redshiftzero <jen@penumbralabs.xyz>
2023-06-05 17:51:38 -0400

    feat: delete all vendored ark-sponge code

--- poseidon377/src/r1cs/vendor.rs
+++
@@ -1,4 +0,0 @@
-// This is the vendored repository from: https://github.com/penumbra-zone/sponge
-// TODO: Replace this with a different R1CS implementation, and then delete
-// this vendored repository.
-pub mod sponge;
