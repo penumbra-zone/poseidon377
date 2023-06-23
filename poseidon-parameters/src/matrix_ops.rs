e87406e7e56bb6647059d4c06bf11b3ff9e066a6
redshiftzero <jen@penumbralabs.xyz>
2023-06-23 14:50:39 -0400

    paramgen: perron's irreducibility criterion (needed for m_i calc)
    We need to be able to compute the minimal polynomial in order
    to check that a randomly generated internal matrix is a secure choice,
    i.e. that it does not have arbitrarily long subspace trails.
    
    We need to be able to compute the minimal polynomial of a matrix
    and check it's irreducible. We can't yet do the former, but with
    Perron's criteron for monic polynomials we can do the latter with
    this commit.

--- poseidon-parameters/src/matrix_ops.rs
+++ poseidon-parameters/src/matrix_ops.rs
@@ -64,6 +64,64 @@ pub fn dot_product<F: PrimeField>(a: &[F], b: &[F]) -> F {
     a.iter().zip(b.iter()).map(|(x, y)| *x * *y).sum()
 }
 
+pub struct Polynomial<F> {
+    /// The coefficients of the polynomial a_0, ..., a_i
+    pub coeffs: Vec<F>,
+}
+
+impl<F: PrimeField> Polynomial<F> {
+    /// Construct a new polynomial
+    pub fn new(coeffs: Vec<F>) -> Self {
+        Self { coeffs }
+    }
+
+    /// Degree of the polynomial
+    pub fn max_degree(&self) -> usize {
+        self.coeffs.len() - 1
+    }
+
+    /// Evaluate the polynomial at a given point
+    pub fn evaluate(&self, x: F) -> F {
+        self.coeffs
+            .iter()
+            .rev()
+            .fold(F::zero(), |acc, coeff| acc * x + coeff)
+    }
+
+    /// Check if the polynomial is irreducible using Perron's irreducibility criterion.
+    pub fn is_irreducible(&self) -> bool {
+        // We first need to check the polynomial is monic.
+        if self.coeffs.last() != Some(&F::one()) {
+            unimplemented!("polynomial is not monic, not sure how to check irreducibility")
+        } else {
+            // The polynomial is monic, so we can apply Perron's criterion.
+            // See https://en.wikipedia.org/wiki/Perron%27s_irreducibility_criterion
+            // for more details.
+            let n = self.max_degree();
+            let mut sum = F::one();
+            for i in 0..n - 1 {
+                sum += self.coeffs[i];
+            }
+
+            match self.coeffs[n - 1] {
+                // Condition 1:
+                // $\abs{a_{n-1}} > 1 + \abs{a_{n-2}} + ... + \abs{a_0}$
+                coeff if coeff > sum => true,
+                // Condition 2:
+                // $\abs{a_{n-1}} = 1 + \abs{a_{n-2}} + ... + \abs{a_0}$
+                // AND
+                // f(1) != 0 AND f(-1) != 0
+                coeff if coeff == sum => {
+                    let f_of_1 = self.evaluate(F::one());
+                    let f_of_neg_1 = self.evaluate(-F::one());
+                    f_of_1 != F::zero() && f_of_neg_1 != F::zero()
+                }
+                _ => false,
+            }
+        }
+    }
+}
+
 /// Matrix operations that are defined on square matrices.
 pub trait SquareMatrixOperations<F> {
     /// Compute the matrix inverse, if it exists
@@ -78,4 +136,20 @@ pub trait SquareMatrixOperations<F> {
     fn cofactors(&self) -> Self;
     /// Compute the matrix determinant
     fn determinant(&self) -> F;
+    /// Compute the minimal polynomial
+    fn min_poly(&self) -> Polynomial<F>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ark_ed_on_bls12_377::Fq;
+
+    #[test]
+    fn poly_evaluate() {
+        // f(x) = 1 + 2x + 3x^2
+        let poly = Polynomial::new(vec![Fq::from(1), Fq::from(2), Fq::from(3)]);
+        assert_eq!(poly.max_degree(), 2);
+        assert_eq!(poly.evaluate(Fq::from(2)), Fq::from(17));
+    }
 }
--- poseidon-parameters/src/v2.rs
+++ poseidon-parameters/src/v2.rs
@@ -5,7 +5,9 @@ pub use crate::arc_matrix::ArcMatrix;
 pub use crate::matrix::SquareMatrix;
 pub use crate::round_numbers::RoundNumbers;
 
-pub use crate::{matrix_ops::MatrixOperations, matrix_ops::SquareMatrixOperations};
+pub use crate::{
+    matrix_ops::MatrixOperations, matrix_ops::Polynomial, matrix_ops::SquareMatrixOperations,
+};
 
 /// A set of Poseidon2 parameters for a given set of input parameters.
 #[derive(Clone, Debug)]
