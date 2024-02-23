9271ce852e339aa6efd3703ac0bc279a5545d3ce
redshiftzero <jen@penumbralabs.xyz>
2024-02-22 23:11:09 -0500

    poseidon-parameters: v1

--- poseidon-parameters/src/v1.rs
+++ poseidon-parameters/src/v1.rs
@@ -1,7 +1,9 @@
 pub use crate::alpha::Alpha;
 pub use crate::round_numbers::RoundNumbers;
 
-pub use crate::{matrix::Matrix, matrix::SquareMatrix, matrix_ops::mat_mul};
+pub use crate::{matrix::Matrix, matrix::SquareMatrix};
+
+// pub use crate::matrix_ops::mat_mul;
 
 pub use crate::{
     arc_matrix::ArcMatrix, arc_matrix::OptimizedArcMatrix, matrix_ops::MatrixOperations,
@@ -9,13 +11,21 @@ pub use crate::{
 };
 
 /// A set of Poseidon1 parameters for a given set of input parameters.
+///
+/// The const `STATE_SIZE` corresponds to $t$ in the paper, the width of the hash function,
+/// e.g. $t=3$ corresponds to a 2-to-1 hash.
 #[derive(Clone, Debug)]
-pub struct PoseidonParameters {
+pub struct PoseidonParameters<
+    const STATE_SIZE: usize,
+    const STATE_SIZE_MINUS_1: usize,
+    const NUM_MDS_ELEMENTS: usize,
+    const NUM_ROUND_ROWS: usize,
+    const NUM_ROUND_COLS: usize,
+    const NUM_ROUND_ELEMENTS: usize,
+> {
     // Input parameters.
     /// Security level.
     pub M: usize,
-    /// Width of desired hash function, e.g. $t=3$ corresponds to a 2-to-1 hash.
-    pub t: usize,
 
     // Generated parameters.
     /// Exponent of the Sbox, i.e. S-box(x) = x^{\alpha} used in the `SubWords` step
@@ -25,14 +35,14 @@ pub struct PoseidonParameters {
     pub rounds: RoundNumbers,
 
     /// `t x t` MDS matrix used in the `MixLayer` step
-    pub mds: MdsMatrix,
+    pub mds: MdsMatrix<STATE_SIZE, STATE_SIZE_MINUS_1, NUM_MDS_ELEMENTS>,
 
     /// `num_total_rounds x t` matrix of constants used in the `AddRoundConstant` step
-    pub arc: ArcMatrix,
+    pub arc: ArcMatrix<NUM_ROUND_ROWS, NUM_ROUND_COLS, NUM_ROUND_ELEMENTS>,
 
     /// Optimized round constants.
-    pub optimized_arc: OptimizedArcMatrix,
-
-    /// Optimized MDS matrices.
-    pub optimized_mds: OptimizedMdsMatrices,
+    pub optimized_arc: OptimizedArcMatrix<NUM_ROUND_ROWS, NUM_ROUND_COLS, NUM_ROUND_ELEMENTS>,
+    // TODO:
+    // /// Optimized MDS matrices.
+    //pub optimized_mds: OptimizedMdsMatrices,
 }
