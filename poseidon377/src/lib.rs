31ac64d71a3ebcc268dca64d97d1c204423a683d
redshiftzero <jen@penumbralabs.xyz>
2024-04-02 17:25:19 -0400

    poseidon377: rate 2

--- poseidon377/src/lib.rs
+++ poseidon377/src/lib.rs
@@ -5,13 +5,14 @@ mod hash;
 mod params;
 
 // pub use hash::{hash_1, hash_2, hash_3, hash_4, hash_5, hash_6, hash_7};
-pub use hash::hash_1;
+pub use hash::{hash_1, hash_2};
 
 /// Parameters for the rate-1 instance of Poseidon.
 pub const RATE_1_PARAMS: PoseidonParameters<2, 1, 4, 1, 39, 2, 78, 31> = params::rate_1::rate_1();
 
 /// Parameters for the rate-2 instance of Poseidon.
-// pub static RATE_2_PARAMS: Lazy<PoseidonParameters<Fq>> = Lazy::new(params::rate_2);
+pub static RATE_2_PARAMS: PoseidonParameters<3, 2, 9, 4, 39, 3, 117, 31> = params::rate_2::rate_2();
+
 // /// Parameters for the rate-3 instance of Poseidon.
 // pub static RATE_3_PARAMS: Lazy<PoseidonParameters<Fq>> = Lazy::new(params::rate_3);
 // /// Parameters for the rate-4 instance of Poseidon.
